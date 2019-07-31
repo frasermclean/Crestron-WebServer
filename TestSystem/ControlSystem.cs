098d75701ea99233c318d06ffe27d081bfcb6e53
Fraser McLean <voltboyee@gmail.com>
2019-07-31 11:56:47 +1000

    Created test system project

---
+++ TestSystem/ControlSystem.cs
@@ -0,0 +1,13 @@
+using System;
+using Crestron.SimplSharp;
+using Crestron.SimplSharpPro;
+using Crestron.SimplSharpPro.CrestronThread;
+using Crestron.SimplSharpPro.Diagnostics;
+using Crestron.SimplSharpPro.DeviceSupport;
+
+namespace TestSystem
+{
+    public class ControlSystem : CrestronControlSystem
+    {
+    }
+}
